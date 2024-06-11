2b7701b8c37b58743f0047cd963c277c59cd3f62
Zahid Rajput <muhammadzahidlatif123@gmail.com>
2024-06-11 11:16:10 +0500

    optmized

--- src/components/Trymore.jsx
+++ src/components/Trymore.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import './Trymore.css';
 const Trymore = () => {
     return (
-        <div className="t-main">
+        <section className="t-main">
             <img className='img-1' src="t-1.jpg" alt="" />
             <img className='img-2' src="t-2.png" alt="" />
-        <div className="t-child">
-        <h1>Try More</h1>
-        <p>
-        Lorem ipsum dolor sit amet, consectetur 
-        adipiscing elit. Morbi vehicula lectus id.
-        </p>
-        <button>
-            Learn more
-        </button>
-        </div>
-        </div>
+            <div className="t-child">
+                <h1>Try More</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. Morbi vehicula lectus id.
+                </p>
+                <button>
+                    Learn more
+                </button>
+            </div>
+        </section>
     );
-    }
-    export default Trymore;
+}
+export default Trymore;
